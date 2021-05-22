394482ae0f43bc5ea72eea9d841c47bcd1aa1e89
Aqua Development⚡⚡ <83559800+Aquaaaaaaaa@users.noreply.github.com>
2021-05-22 11:49:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const colors = require('colors')
 const config =   require('./config.json')
 const core = require('./Utility/core.js');
 
+(async () => {
+    client.commands = new Discord.Collection();
+    client.aliases = new Discord.Collection();
+    await core.registerCommands(client, '../commands/');+
+    await core.registerEvents(client, '../events/');
+})();
+
 client.on('ready', () => {
     console.log('Successfully Logged in as: '.cyan + client.user.tag + 'Bot has been Coded by Aqua!'.blue)
 });
