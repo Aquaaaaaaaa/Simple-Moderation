9c3464e27f53f36867ce0735c5e1afdb0e5d90dc
Aquatic Development <83559800+Aquaaaaaaaa@users.noreply.github.com>
2021-05-25 19:08:43 +0200

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,43 +1,50 @@
-const Discord = require('discord.js')
+const { MessageCollector } = require('discord.js')
+const Discord = require('discord.js');
+const { read } = require('fs');
 
 module.exports = {
     name: "ban",
-    execute: async (client, message, args) => {
-        let nopermsembed = new Discord.MessageEmbed()
-        .setTitle('Oops.. No Permission to use This Command!')
-        .setDescription('Your missing the Permission: **BAN_MEMBERS**')
+  execute: async (client, message, args) => {
+      
+
+    let NoPermissionEmbed = new Discord.MessageEmbed()
+    .setTitle("Oops..")
+    .setDescription('Your missing the Permission: `BAN_MEMBERS`')
+    .setTimestamp()
+    .setFooter('ðŸŒŠ Aquatic ')
+    .setColor("#6ef6fa")
+    
+    let NoMentionEmbed = new Discord.MessageEmbed()
+    .setTitle("Oops..")
+    .setDescription('You Forgot to mention a User / The user is Invalid')
+    .setTimestamp()
+    .setFooter('ðŸŒŠ Aquatic ')
+    .setColor("#6ef6fa")
+
+    let perms = message.member.hasPermission("BAN_MEMBERS")
+    if (!perms) return message.channel.send(NoPermissionEmbed)
+
+    let user = message.mentions.members.first()
+    if (!user) return message.channel.send(NoMentionEmbed)
+
+    let reason = args.slice(1).join(" ")
+    if(!reason) reason = "No Reason"
+
+        let UserDMEmbed = new Discord.MessageEmbed()
+        .setDescription("You have been **Kicked** in " + message.guild.name + ` By ${message.author} for ` + reason)
+        .setFooter('Have a Nice Day!')
         .setTimestamp()
-        .setFooter('Coded by Aqua!')
-
-        let nomentionembed = new Discord.MessageEmbed()
-        .setTitle('Oops... You have not Mentioned any User!')
-        .setDescription('Please Mention a User!')
-        .setTimestamp()
-        .setFooter('Coded by Aqua!')
-
-        let perms = message.members.hasPermission("BAN_MEMBERS")
-        if(!perms) return message.channel.send(nopermsembed)
-
-        let user = message.mentions.members.first()
-        if(!user) return message.channel.send(nomentionembed)
-        
-        let reason = args.slice(1).join(" ")
-        if(!reason) reason = "No Reason Specified!"
-
-        let userdmembed = new Discord.MessageEmbed()
-        .setDescription('You have been **BANNED** in ' + message.guild.name + `By ${message.author} for ` +reason)
-        .setTimestamp()
-        .setFooter('Coded by Aqua!')
+        .setFooter('ðŸŒŠ Aquatic ')
+        .setColor("#6ef6fa")
         try {
-            await user.send(userdmembed)
-        } catch (err){
+            await user.send(UserDMEmbed)
+        } catch (err) {
             console.log(err)
         }
-
+        
         await user.ban()
 
-        let userbanembed = new Discord.MessageEmbed()
+        let UserBanTrue = new Discord.MessageEmbed()
         .setDescription(`Successfully **Banned** ${user}`)
-        message.channel.send(userbanembed)
-    }
-}
+        message.channel.send(UserBanTrue)
+  }}
